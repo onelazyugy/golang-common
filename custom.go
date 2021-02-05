0aee31c7b2e7eb15b945138ecef626a569fd1f42
Viet Le <vietle@SVUSLP01019.attlocal.net>
2021-02-04 22:24:30 -0500

    Added add function for release 3.0.1

--- custom.go
+++ custom.go
@@ -4,13 +4,3 @@ package common
 func Add(a int, b int) int {
 	return a + b
 }
-
-// Subtract two number
-func Subtract(a int, b int) int {
-	return a - b
-}
-
-// Divide two number
-func Divide(a int, b int) int {
-	return a / b
-}
