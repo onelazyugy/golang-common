617128789815ed22e2751f3272fc03906580fd77
Viet Le <vietle@SVUSLP01019.attlocal.net>
2021-02-05 10:45:57 -0500

    only Add is left

--- custom.go
+++ custom.go
@@ -4,18 +4,3 @@ package common
 func Add(a int, b int) int {
 	return a + b
 }
-
-// Subtract two numbers
-func Subtract(a int, b int) int {
-	return a - b
-}
-
-// Divide two numbers
-func Divide(a int, b int) int {
-	return a / b
-}
-
-//Multiply two numbers
-func Multiply(a int, b int) int {
-	return a * b
-}
